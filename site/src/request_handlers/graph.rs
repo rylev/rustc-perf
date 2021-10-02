23d1c5c22c44034518e25316cbea2bb7ff23acf4
Ryan Levick <ryan.levick@gmail.com>
2021-10-02 12:47:12 +0200

    Add endpoint for single graph

--- site/src/request_handlers/graph.rs
+++ site/src/request_handlers/graph.rs
@@ -2,25 +2,31 @@ use collector::Bound;
 use std::collections::HashMap;
 use std::sync::Arc;
 
-use crate::api::graph::GraphKind;
-use crate::api::{graph, ServerResult};
+use crate::api::graphs::GraphKind;
+use crate::api::{graph, graphs, ServerResult};
 use crate::db::{self, ArtifactId, Benchmark, Profile, Scenario};
 use crate::interpolate::IsInterpolated;
 use crate::load::SiteCtxt;
 use crate::selector::{Query, Selector, SeriesResponse, Tag};
 
-pub async fn handle_graphs(
-    body: graph::Request,
-    ctxt: &SiteCtxt,
-) -> ServerResult<Arc<graph::Response>> {
+pub async fn handle_graph(body: graph::Request, ctxt: &SiteCtxt) -> ServerResult<graph::Response> {
     log::info!("handle_graph({:?})", body);
 
+    create_graph(body, ctxt).await
+}
+
+pub async fn handle_graphs(
+    body: graphs::Request,
+    ctxt: &SiteCtxt,
+) -> ServerResult<Arc<graphs::Response>> {
+    log::info!("handle_graphs({:?})", body);
+
     let is_default_query = body
-        == graph::Request {
+        == graphs::Request {
             start: Bound::None,
             end: Bound::None,
             stat: String::from("instructions:u"),
-            kind: graph::GraphKind::Raw,
+            kind: graphs::GraphKind::Raw,
         };
 
     if is_default_query {
@@ -30,7 +36,7 @@ pub async fn handle_graphs(
         }
     }
 
-    let resp = graph_response(body, ctxt).await?;
+    let resp = create_graphs(body, ctxt).await?;
 
     if is_default_query {
         ctxt.landing_page.store(Arc::new(Some(resp.clone())));
@@ -38,13 +44,33 @@ pub async fn handle_graphs(
 
     Ok(resp)
 }
+async fn create_graph(body: graph::Request, ctxt: &SiteCtxt) -> ServerResult<graph::Response> {
+    let artifact_ids = artifact_ids_for_range(ctxt, body.start, body.end);
+    let mut series_iterator = ctxt
+        .statistic_series(
+            Query::new()
+                .set::<String>(Tag::Benchmark, Selector::One(body.benchmark))
+                .set::<String>(Tag::Profile, Selector::One(body.profile))
+                .set::<String>(Tag::Scenario, Selector::One(body.scenario))
+                .set::<String>(Tag::Metric, Selector::One(body.metric)),
+            Arc::new(artifact_ids),
+        )
+        .await?
+        .into_iter()
+        .map(SeriesResponse::interpolate);
 
-async fn graph_response(
-    body: graph::Request,
+    let result = series_iterator.next().unwrap();
+    let graph_series = graph_series(result.series, body.kind);
+    Ok(graph::Response {
+        series: graph_series,
+    })
+}
+
+async fn create_graphs(
+    body: graphs::Request,
     ctxt: &SiteCtxt,
-) -> ServerResult<Arc<graph::Response>> {
-    let range = ctxt.data_range(body.start..=body.end);
-    let commits: Arc<Vec<_>> = Arc::new(range.into_iter().map(|c| c.into()).collect());
+) -> ServerResult<Arc<graphs::Response>> {
+    let artifact_ids = Arc::new(artifact_ids_for_range(ctxt, body.start, body.end));
     let metric_selector = Selector::One(body.stat);
     let mut benchmarks = HashMap::new();
 
@@ -55,7 +81,7 @@ async fn graph_response(
                 .set::<String>(Tag::Profile, Selector::All)
                 .set::<String>(Tag::Scenario, Selector::All)
                 .set::<String>(Tag::Metric, metric_selector.clone()),
-            commits.clone(),
+            artifact_ids.clone(),
         )
         .await?
         .into_iter()
@@ -75,12 +101,12 @@ async fn graph_response(
             .insert(scenario, graph_series);
     }
 
-    let summary_benchmark = create_summary(ctxt, metric_selector, &commits, body.kind).await?;
+    let summary_benchmark = create_summary(ctxt, metric_selector, &artifact_ids, body.kind).await?;
 
     benchmarks.insert("Summary".to_string(), summary_benchmark);
 
-    Ok(Arc::new(graph::Response {
-        commits: Arc::try_unwrap(commits)
+    Ok(Arc::new(graphs::Response {
+        commits: Arc::try_unwrap(artifact_ids)
             .unwrap()
             .into_iter()
             .map(|c| match c {
@@ -92,6 +118,11 @@ async fn graph_response(
     }))
 }
 
+fn artifact_ids_for_range(ctxt: &SiteCtxt, start: Bound, end: Bound) -> Vec<ArtifactId> {
+    let range = ctxt.data_range(start..=end);
+    range.into_iter().map(|c| c.into()).collect()
+}
+
 /// Creates a summary "benchmark" that averages the results of all other
 /// test cases per profile type
 async fn create_summary(
@@ -99,7 +130,7 @@ async fn create_summary(
     metric_selector: Selector<String>,
     commits: &Arc<Vec<ArtifactId>>,
     graph_kind: GraphKind,
-) -> ServerResult<HashMap<Profile, HashMap<String, graph::Series>>> {
+) -> ServerResult<HashMap<Profile, HashMap<String, graphs::Series>>> {
     let mut baselines = HashMap::new();
     let mut summary_benchmark = HashMap::new();
     let summary_query_cases = iproduct!(
@@ -157,8 +188,8 @@ async fn create_summary(
 fn graph_series(
     points: impl Iterator<Item = ((ArtifactId, Option<f64>), IsInterpolated)>,
     kind: GraphKind,
-) -> graph::Series {
-    let mut graph_series = graph::Series {
+) -> graphs::Series {
+    let mut graph_series = graphs::Series {
         points: Vec::new(),
         interpolated_indices: Default::default(),
     };
